b934510dab72d6f78ed2890ca2449bb5b74aa40d
Jesuva <jesuva.2308@gmail.com>
2021-05-07 11:55:28 +0530

    initial commit

---
+++ src/main/java/gcp/com/config/WebConfig.java
@@ -0,0 +1,11 @@
+package gcp.com.config;
+
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
